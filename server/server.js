f0c326bc566f0b0a6e4dd084c35370e3ab7222a8
Dave Guymon <davidwguymon@gmail.com>
2017-09-13 17:05:47 -0600

    Successfully implemented PassportJS for authentication with Facebook.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,89 @@
 const express = require('express');
 const passport = require('passport');
-const Strategy = require('passport-facebook').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const config = require('./../config');
 const port = 3030;
 
+// Create a new Express application.
 const app = express();
+
+// Configure the Facebook strategy for use by Passport.
+passport.use(new FacebookStrategy({
+  clientID: config.FACEBOOK_APP_ID,
+  clientSecret: config.FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:3030/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  return cb(null, profile);
+}));
+
+
+
+// Configure Passport authenticated session persistence.
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+// Configure view engine to render EJS templates.
+app.set('view', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes.
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log(req.user)
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:3000/');
+  });
+
+app.get('/me',
+function(req, res) {
+  res.send(req.user)
+})
+
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+  app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+  app.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+  app.get('/login/facebook/return',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+  app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
 
 app.listen( port, ()=> {console.log(`Listening on port: ${port}.`)});
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-export default function Header(props) {
+export default class Header extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      name: ''
+    }
+  }
 
-  return (
-    <main>
-      <h1>HEADER COMPONENT</h1>
-    </main>
-  )
+  componentDidMount(){
+    axios.get('/me').then(res => {
+      console.log(res.data)
+      this.setState({
+        name: res.data.displayName
+      })
+    })
+  }
+
+  render(){
+    return (
+      <main>
+        <h1>HEADER COMPONENT</h1>
+        <a href="http://localhost:3030/login/facebook"><p>LOG IN</p></a>
+        <p>Hello, {this.state.name}</p>
+      </main>
+    )
+  }
 }
